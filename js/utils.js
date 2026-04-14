bcb14a9d59a4c3f7077f6a279b1ffa52e933b494
Mayagung <agungsaefulloh23@gmail.com>
2026-04-14 22:46:58 +0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,10 @@
- 
+function formatRp(angka) {
+    return "Rp " + new Intl.NumberFormat("id-ID").format(angka);
+}
+
+function showToast(msg) {
+    const toast = document.getElementById("toast");
+    toast.innerText = msg;
+    toast.classList.add("show");
+    setTimeout(() => toast.classList.remove("show"), 2000);
+}
